9802d3ccdacd6bd51eb434e1e620151b1268a82e
Gabriel Takaki <gabitj@icloud.com>
2022-02-08 18:07:50 -0300

    exercise 2

--- exercise.py
+++ exercise.py
@@ -1,11 +1,18 @@
 # 1
-
 def bigger_number(a, b):
-    if (a > b):
+    if a > b:
         return a
-    elif (b > a):
+    elif b > a:
         return b
     return 'Numbers are equals'
 
 
 print(bigger_number(3, 3))
+
+
+# 2
+def average(lst):
+    return sum(lst) / len(lst)
+
+
+print(average([4, 4, 10]))
