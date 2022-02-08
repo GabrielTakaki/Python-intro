4af215dea99e49a9c6c6d0634b2d8e123808271d
Gabriel Takaki <gabitj@icloud.com>
2022-02-08 15:08:23 -0300

    range

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ a = 5j
 print(type(a))
 
 
-# Listas
+# Lists
 fruits = ['orange', 'grape', 'apple', 'pineapple']
 print(fruits[0])
 print(fruits[-1])
@@ -39,3 +39,46 @@ print(trybe_course)
 
 trybe_course[0] = 'Fundamentos'
 print(trybe_course)
+
+
+# Tuples
+# Similar to lists, however can not be modified during the program execution
+
+user = ('Cassio', 'Botaro', 42)
+user[0]
+
+
+# Set
+# Unique and non-ordered elements.
+permissions = {"member", "group"}
+
+permissions.add("root")
+permissions.add("member")  # As this element already exists, nothing happens.
+
+print(permissions.union({"user"}))
+print(permissions.intersection({"user", "member"}))
+print(permissions.difference({"user"}))
+
+
+# Immutable sets (frozenset)
+permissions = frozenset(['member', 'group'])
+print(permissions.union({'user'}))
+print(permissions.intersection({'user', 'member'}))
+print(permissions.difference({"user"}))
+
+
+# Dictionaries (dict)
+# Structure that associates a key to a determent value
+people_by_id = {1: 'Cassio', 2: 'Joao', 3: 'Felipe'}
+people_by_name = {'Cassio': 1, 'Joao': 2, 'Felipe': 3}
+
+print(people_by_id[1])  # Cassio
+del people_by_id[1]
+print(people_by_name.items())
+
+
+# Range
+print(range(5))
+print(range(1, 6))
+print(range(1, 11, 2))  # output: [1, 3, ,5 ,7 , 9]
+print(range(10, 0, -1))  # output: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
