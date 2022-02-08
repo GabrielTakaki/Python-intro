d13f1b4d8e0b3dd0270bc8441a4aeec7455f0597
Gabriel Takaki <gabitj@icloud.com>
2022-02-08 14:31:08 -0300

    python list

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ print(3 + 4j)
 print((3 + 4j) + 4)
 a = 5j
 print(type(a))
+
+
+# Listas
+fruits = ['orange', 'grape', 'apple', 'pineapple']
+print(fruits[0])
+print(fruits[-1])
+
+fruits.append('banana')
+fruits.remove('pineapple')
+fruits.extend(['pear', 'melon', 'kiwi'])
+
+print(fruits.index('apple'))
+print(fruits)
+print(fruits.sort())
