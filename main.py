290057ea806a57ef7f9e57de41acbb30a90b1cee
Gabriel Takaki <gabitj@icloud.com>
2022-02-08 15:31:50 -0300

    for

--- main.py
+++ main.py
@@ -106,3 +106,36 @@ from_to = {
     "lastName": "last_name",
 }
 from_to[key]
+
+
+# Repetition structures
+
+# 1- For
+restaurants = [
+    {"name": "Restaurante A", "nota": 4.5},
+    {"name": "Restaurante B", "nota": 3.0},
+    {"name": "Restaurante C", "nota": 4.2},
+    {"name": "Restaurante D", "nota": 2.3},
+]
+
+filtered_restaurants = []
+min_rating = 3.0
+for restaurant in restaurants:
+    if restaurant["nota"] > min_rating:
+        filtered_restaurants.append(restaurant)
+print(filtered_restaurants)
+
+for index in range(5):
+    print(index)
+
+
+# List comprehension
+# Better way to solve the problem
+min_rating = 3.0
+filtered_restaurants = [restaurant['name']
+                         for restaurant in restaurants
+                         if restaurant["nota"] > min_rating]
+print(filtered_restaurants)
+
+
+# While
