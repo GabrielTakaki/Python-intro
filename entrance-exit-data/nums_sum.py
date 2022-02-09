e7152e452f0f7a6f57dc02dac3b5ef1d9471ea3a
Gabriel Takaki <gabitj@icloud.com>
2022-02-09 11:34:04 -0300

    venv

---
+++ entrance-exit-data/nums_sum.py
@@ -0,0 +1,16 @@
+# Packages
+import http  # Import the http package module
+from http import client  # Import the module client from http package
+from http.client import HTTP_PORT  # Import the constant HTTP_PORT from the client module from http package
+
+
+# Virtual environment
+# sudo apt install python3-venv
+# python3 -m venv .venv
+# source .venv/bin/activate
+# which python
+
+
+my_number = input("Write your number: ")
+print(my_number)
+
