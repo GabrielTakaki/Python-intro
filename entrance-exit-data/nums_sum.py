6c3d7af3e1de2fed8ec090f549f8cb107a95ad86
Gabriel Takaki <gabitj@icloud.com>
2022-02-09 11:42:28 -0300

    entrance of data. lucky number

--- entrance-exit-data/nums_sum.py
+++ entrance-exit-data/nums_sum.py
@@ -10,11 +10,20 @@ from http.client import HTTP_PORT  # Import the constant HTTP_PORT from the clie
 # source .venv/bin/activate
 # which python
 
+import random
 
 my_number = 0
-
 while my_number < 42:
     # Input comes as str. Change to int to sum.
     my_number += int(input("Write your number: "))
 print("Your number sum is greater then 42")
 
+
+# Entrance
+random_number = random.randint(1, 10)  # Chose a number between 1 and 0
+guess = ""
+
+while guess != random_number:  # While the number isn't write
+    guess = int(input("What's your hint? "))  # Ask the user a number
+
+print("The lucky number was: ", guess)
