caf472e94fd9637a0b9e04d43e1f994127e2fa0f
Gabriel Takaki <gabitj@icloud.com>
2022-02-09 18:31:32 -0300

    ex 4

--- data-input-output/exercises.py
+++ data-input-output/exercises.py
@@ -24,3 +24,29 @@ if guess == random_word:
     print('Parabens, voce acertou a palavra!')
 else:
     print(f'Que pena! a palavra era {random_word}')
+
+
+# 3.
+with open('words.txt', mode='w') as file:
+    words = ['room ', 'acquaintance ', 'friend ', 'mattress']
+    file.writelines(words)
+
+with open('words.txt', mode='r') as file:
+    for line in file:
+        split_words = line.split(' ')
+        print(split_words)
+        random_w = random.choice(split_words)
+        scrambled = ''.join(random.sample(random_w, len(random_w)))
+        guessed = ''
+        count = 0
+        print(scrambled)
+        while guessed != random_w and count < 3:
+            count += 1
+            guessed = input('Guess the word written above: ')
+
+        if guessed == random_w:
+            print('Parabens, voce acertou a palavra!')
+        else:
+            print(f'Que pena! a palavra era {random_w}')
+
+
